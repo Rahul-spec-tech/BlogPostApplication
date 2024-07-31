858aa6d5b559d5fbda93f544ed392717ff7fd46e
Rahul-spec-tech <rahulrn24cse@srishakthi.ac.in>
2024-07-31 18:31:39 +0530

    PostAPI
    Created the API for Post and connected with the MongoDB Server with default port 27107

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validator.isEmail, 'Invalid email']
+    },
+    phoneNum: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    }
+});
+module.exports = mongoose.model('User', userSchema);
+module.exports=User;
